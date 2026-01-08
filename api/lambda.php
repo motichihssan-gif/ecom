0bf71c975405e65b1141243cb926edb097642065
Ihsane Motich <motichihssan@gmail.com>
2026-01-08 17:42:31 +0100

    atelier3

--- api/lambda.php
+++ api/lambda.php
@@ -1,16 +1,20 @@
 <?php
-
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
+require __DIR__ . '/../vendor/autoload.php';
 
-require __DIR__.'/../vendor/autoload.php';
+/** @var Application $app */
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-$app->handleRequest(Request::capture());
+$response = $kernel->handle(
+    $request = Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
